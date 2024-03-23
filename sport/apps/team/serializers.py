1a12ef1a34feedd77f6ac8db0b0b8fe650b18e2f
Wenderson-Oscar <wendersonoscardev@gmail.com>
2024-03-23 19:19:39 -0300

    validate limit

--- sport/apps/team/serializers.py
+++ sport/apps/team/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from sport.apps.team.models import Team
 from sport.apps.accounts.serializers import PlayerSerializer, TrainerSerializer, Player, Trainer
+from django.core.exceptions import ValidationError
 
 
 class CreateTeamSerializer(serializers.ModelSerializer):
@@ -20,6 +21,25 @@ class CreateTeamSerializer(serializers.ModelSerializer):
             'players_limit': {'write_only': True, 'required': False},
             'trainers_limit': {'write_only': True, 'required': False}
         }
+
+    def validate(self, data):
+        player_limit = data.get(
+            'players_limit', self.instance.players_limit if self.instance else None)
+        trainer_limit = data.get(
+            'trainers_limit', self.instance.trainers_limit if self.instance else None)
+        current_players = self.instance.players.all() if self.instance else []
+        current_trainers = self.instance.trainers.all() if self.instance else []
+        proposed_players = data.get('players', current_players)
+        proposed_trainers = data.get('trainers', current_trainers)
+        total_players = len(set(proposed_players) | set(current_players))
+        total_trainers = len(set(proposed_trainers) | set(current_trainers))
+        if player_limit is not None and total_players > player_limit:
+            raise ValidationError(
+                {'players': f'A quantidade de jogadores excede o limite permitido: ({player_limit}).'})
+        if trainer_limit is not None and total_trainers > trainer_limit:
+            raise ValidationError(
+                {'trainers': f'A quantidade de treinadores excede o limite permitido: ({trainer_limit}).'})
+        return data
 
     def update(self, instance, validated_data):
         for attr, value in validated_data.items():
